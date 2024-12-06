d292d7a9f92460c0b1cb972a469f2080fa3a2648
nikezwe <mnikezwe@gmail.com>
2024-12-06 06:51:48 -0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,34 @@
+// contact.js
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+document.getElementById('form').addEventListener('submit', async (e) => {
+    e.preventDefault(); // Empêche le rechargement de la page
+
+    // Récupérer les valeurs du formulaire
+    const nom = document.getElementById('nom').value;
+    const prenom = document.getElementById('prenom').value;
+    const telephone = document.getElementById('telephone').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    console.log("Nom:", nom, "Email:", email, "Message:", message);
+    try {
+        // Ajouter un document à la collection "contacts"
+        const docRef = await addDoc(collection(window.db, "contact"), {
+            nom: nom,
+            prenom: prenom,
+            telephone: telephone,
+            email: email,
+            message: message,
+            timestamp: new Date() // Ajoute un horodatage
+        });
+        console.log("Document écrit avec ID: ", docRef.id);
+        
+        alert("Login successuf!");
+    } catch (e) {
+        console.error("Erreur lors de l'ajout du document: ", e);
+        alert("Une erreur s'est produite lors de l'envoi de votre message : " + e.message);
+    }
+
+    // Réinitialiser le formulaire
+    document.getElementById('form').reset();
+});
---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// // firebase-config.js
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC6wgloeK6Ht0B9tKoxZTBBvVF704VBkUI",
+//     authDomain: "jcommerce-66ef3.firebaseapp.com",
+//     projectId: "jcommerce-66ef3",
+//     storageBucket: "jcommerce-66ef3.firebasestorage.app",
+//     messagingSenderId: "1089413034206",
+//     appId: "1:1089413034206:web:9d8b0183bca1137adfd24f",
+//     measurementId: "G-W8L9GCSGWV"
+// };
+
+// // Initialiser Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// export { db };
